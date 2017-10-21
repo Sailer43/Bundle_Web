bbc3205871840ac157cf8585bb8ad27933f12e8f
Sitao Chen <sitaochen@Sitaos-MacBook-Air.local>
2017-10-21 14:16:44 -0400

    changed bundle.html and .js

--- src/scripts/bundler.js
+++ src/scripts/bundler.js
@@ -4,10 +4,23 @@ $(function() {
         Item:<input class="item">
         <p>Quantity:<input class="count"></p>
       </li>`;
-    
-    $("#confirmButton").on("click", function() {
+
+    function changeView() {
         $("#secondPart").toggle(500);
         $("#firstPart").toggle(500);
+    }
+    $("#backButton").on("click", function() {
+       changeView();
+    });
+
+    $("#BundleName").on("keypress", function(event) {
+        if (event.which == 13) {
+            changeView();
+        }
+    })
+
+    $("#confirmButton").on("click", function() {
+        changeView()
         var bundleName = $("#BundleName").val();
         console.log(bundleName);
     });
